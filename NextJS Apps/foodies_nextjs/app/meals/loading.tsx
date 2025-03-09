7ff0b51e55a8662f78179968e6b905c7d81ec304
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-09 10:53:49 -0700

    loading page

---
+++ NextJS Apps/foodies_nextjs/app/meals/loading.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import classes from "./loading.module.css";
+export default function MealsLoadingPage() {
+  return <div className={classes.loading}>Fetching Meals ...</div>;
+}
