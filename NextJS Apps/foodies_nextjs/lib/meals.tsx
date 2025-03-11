1f9a8d082f80828bde6f9c2920c3f00bd9f96b9f
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-10 19:23:41 -0700

    add meals details and fetch single meal

--- NextJS Apps/foodies_nextjs/lib/meals.tsx
+++ NextJS Apps/foodies_nextjs/lib/meals.tsx
@@ -6,3 +6,18 @@ export const getMeals = async () => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
   return db.prepare("SELECT * FROM meals").all();
 };
+
+type Meal = {
+  title: string;
+  image: string;
+  summary: string;
+  instructions: string;
+  creator: string;
+  creator_email: string;
+};
+
+export const getMeal = (slug: string): Meal => {
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  return db.prepare("SELECT * FROM meals WHERE slug=?").get(slug);
+};
