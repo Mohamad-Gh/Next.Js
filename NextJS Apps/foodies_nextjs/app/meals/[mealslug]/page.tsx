de903c611bc1e181388de24a35cf65ae376ea237
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-10 19:27:11 -0700

    call notFound function

--- NextJS Apps/foodies_nextjs/app/meals/[mealslug]/page.tsx
+++ NextJS Apps/foodies_nextjs/app/meals/[mealslug]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./page.module.css";
 import Image from "next/image";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({
   params,
@@ -9,6 +10,10 @@ export default function MealDetailsPage({
   params: { mealslug: string };
 }) {
   const meal = getMeal(params.mealslug);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
