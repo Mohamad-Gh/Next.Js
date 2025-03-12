eae8257690152c469cf71a16d7e0fea0d6d69528
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-11 21:47:26 -0700

    slugify and xss

--- NextJS Apps/foodies_nextjs/lib/meals.tsx
+++ NextJS Apps/foodies_nextjs/lib/meals.tsx
@@ -1,5 +1,6 @@
 import sql from "better-sqlite3";
-
+import slugify from "slugify";
+import xss from "xss";
 const db = sql("meals.db");
 
 export const getMeals = async () => {
@@ -8,6 +9,7 @@ export const getMeals = async () => {
 };
 
 type Meal = {
+  slug: string;
   title: string;
   image: string;
   summary: string;
@@ -20,4 +22,9 @@ export const getMeal = (slug: string): Meal => {
   // await new Promise((resolve) => setTimeout(resolve, 5000));
 
   return db.prepare("SELECT * FROM meals WHERE slug=?").get(slug);
+};
+
+export const shareMeal = (meal: Meal) => {
+  meal.slug = slugify(meal.title);
+  meal.instructions = xss(meal.instructions);
 };
