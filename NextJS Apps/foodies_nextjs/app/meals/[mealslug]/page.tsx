a49d4a3ae1117319276a6bbe7e055c8b3a959244
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-13 11:09:30 -0700

    metadata for dynamic pages and using notFound function

--- NextJS Apps/foodies_nextjs/app/meals/[mealslug]/page.tsx
+++ NextJS Apps/foodies_nextjs/app/meals/[mealslug]/page.tsx
@@ -4,6 +4,21 @@ import Image from "next/image";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
 
+export const generateMetaData = async ({
+  params,
+}: {
+  params: { mealslug: string };
+}) => {
+  const meal = getMeal(params.mealslug);
+  if (!meal) {
+    notFound();
+  }
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+};
+
 export default function MealDetailsPage({
   params,
 }: {
