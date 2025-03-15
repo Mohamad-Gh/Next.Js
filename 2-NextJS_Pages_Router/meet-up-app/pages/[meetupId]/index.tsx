9d077a2b8b9e515a017d78ea7898277484f393ff
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-15 11:15:15 -0700

    add meetupdetailpage and component

--- 2-NextJS_Pages_Router/meet-up-app/pages/[meetupId]/index.tsx
+++ 2-NextJS_Pages_Router/meet-up-app/pages/[meetupId]/index.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import MeetupDetail from "@/component/meetups/MeetupDetail";
 
 export default function MeetupDetailsPage() {
-  return <div>Details Page</div>;
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup"
+    />
+  );
 }
