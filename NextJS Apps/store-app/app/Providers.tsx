92078d54642655fbc3f13a8e09a020423fbcc160
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-23 11:32:13 -0700

    set up theme-provider and add it to Providers as a main provider

--- NextJS Apps/store-app/app/Providers.tsx
+++ NextJS Apps/store-app/app/Providers.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { ThemeProvider } from "./theme-provider";
+
 function Providers({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <ThemeProvider>{children}</ThemeProvider>
+    </>
+  );
 }
 
 export default Providers;
