ddda5c3555f3b7d1ca52e1af2c2dd9d9895653e9
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-30 13:16:33 -0700

    search as querry params

--- NextJS Apps/store-app/utils/actions.ts
+++ NextJS Apps/store-app/utils/actions.ts
@@ -8,8 +8,30 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({ search = "" }: { search: string }) => {
   return db.product.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+        {
+          company: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+        {
+          description: {
+            contains: search,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
     orderBy: {
       createdAt: "desc",
     },
