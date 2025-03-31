24213fe1591a2bcf909661feffa01f30db6afb99
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-31 11:59:08 -0700

    conosle log to pass current errors to build fast

--- NextJS-Apps/store-app/components/products/FavoriteToggleButton.tsx
+++ NextJS-Apps/store-app/components/products/FavoriteToggleButton.tsx
@@ -1,6 +1,7 @@
 import { FaHeart } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 function FavoriteToggleButton({ productId }: { productId: string }) {
+  console.log("productId", productId);
   return (
     <Button size="icon" variant="outline" className="p-2 cursor-pointer">
       <FaHeart />
