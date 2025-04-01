95f9028264f9c7b8369727aeabd063c4a091bacc
Mohamad-Gh <ghalebizadem@gmail.com>
2025-03-31 18:32:03 -0700

    shorter and cleaner version

--- NextJS-Apps/store-app/components/navbar/SingOutLink.tsx
+++ NextJS-Apps/store-app/components/navbar/SingOutLink.tsx
@@ -4,15 +4,13 @@ import { SignOutButton } from "@clerk/nextjs";
 import { toast } from "sonner";
 
 function SingOutLink() {
+  const handleSingOut = () => {
+    toast("Loged out successfully");
+  };
+
   return (
     <SignOutButton>
-      <Link
-        href={"/"}
-        className="w-full text-left"
-        onClick={() => {
-          toast("Loged out successfully");
-        }}
-      >
+      <Link href={"/"} className="w-full text-left" onClick={handleSingOut}>
         Sign Out
       </Link>
     </SignOutButton>
